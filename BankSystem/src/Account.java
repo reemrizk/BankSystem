b5275ae2acaa76554fb18777fb3f39f3980e8331
reemrizk <72882201+reemrizk@users.noreply.github.com>
2024-01-31 00:17:13 -0500

    Update Account.java

--- BankSystem/src/Account.java
+++ BankSystem/src/Account.java
@@ -3,6 +3,17 @@ public abstract class Account {
 	protected float balance;
 	
 	Account(float balance){
+		this.balance = balance;
+	}
+	
+	
+	public float getBalance() {
+		return balance;
+	
+	}
+	
+	public void deposit(float amount) {
+		balance += amount;
 	}
 
 }
